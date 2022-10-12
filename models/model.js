9f236c4888d6abdffe206b8cce030f6b66f463fb
Mariano Z <marianoz@zoho.com>
2022-10-12 15:40:49 -0300

    feat: cleanup repo

--- models/model.js
+++ models/model.js
@@ -10,10 +10,10 @@ const dataSchema = new mongoose.Schema({
     type: Number,
   },
   studies: {
-    unique: false,
-    institution: { type: String, unique: false, required: false },
-    years: { type: String, unique: false, required: false },
-    description: { type: String, unique: false, required: false },
+      unique: false,
+      institution: { type: String, unique: false, required: false },
+      years: { type: String, unique: false, required: false },
+      description: { type: String, unique: false, required: false },
   },
   workexperience: {
     company: { type: String, unique: false, required: false },
